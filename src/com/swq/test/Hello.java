ab8110c27f3563cc6981445fb0f93a543e2bddd3
swq <swq@qq.com>
2020-05-05 18:06:11 +0800

    本地第四次修改了hello.java

--- src/com/swq/test/Hello.java
+++ src/com/swq/test/Hello.java
@@ -3,5 +3,6 @@ package com.swq.test;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello  edit by local 12345678999999999");
+        System.out.println("程序员添加了一行输出");
     }
 }
