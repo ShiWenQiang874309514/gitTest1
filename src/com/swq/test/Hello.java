7f62385e18cc86e0715446ac612f15d9aa400414
swq <swq@qq.com>
2020-05-05 19:59:04 +0800

    本地第八次提交

--- src/com/swq/test/Hello.java
+++ src/com/swq/test/Hello.java
@@ -2,6 +2,6 @@ package com.swq.test;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello  edit by local 12344321");
+        System.out.println("hello  edit by local 123443211234");
     }
 }
